b969111c1b626a93e08996ec5b78253fd98cc3b4
Sekhar <sekharbag07@gmail.com>
2026-06-16 20:02:09 +0530

    Add teacher logout

--- lib/features/teacher/screens/teacher_dashboard.dart
+++ lib/features/teacher/screens/teacher_dashboard.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../auth/screens/login_screen.dart';
 
 class TeacherDashboard extends StatelessWidget {
   const TeacherDashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Teacher Dashboard', style: TextStyle(fontSize: 24)),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Teacher Dashboard"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+
+              if (!context.mounted) return;
+
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const LoginScreen(),
+                ),
+                (route) => false,
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'Teacher Dashboard',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
