63f44db6ee7a02932fe24ffb3de81a036434325c
Sekhar <sekharbag07@gmail.com>
2026-06-16 20:05:24 +0530

    Add logout to all dashboards

--- lib/features/parent/screens/parent_dashboard.dart
+++ lib/features/parent/screens/parent_dashboard.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../auth/screens/login_screen.dart';
 
 class ParentDashboard extends StatelessWidget {
   const ParentDashboard({super.key});
 
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+
+    if (!context.mounted) return;
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const LoginScreen(),
+      ),
+      (route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Parent Dashboard', style: TextStyle(fontSize: 24)),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Parent Dashboard"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => logout(context),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'Parent Dashboard',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
