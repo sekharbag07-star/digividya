fd024f97ae29126182641d76112a91fcf43808dc
sekharbag07-star <sekharbag07@gmail.com>
2026-06-20 00:05:04 +0530

    Remove legacy admin dashboard files

--- lib/features/admin/widgets/admin_widgets.dart
+++
@@ -1,4 +0,0 @@
-export '../dashboard/widgets/dashboard_header.dart';
-export '../dashboard/widgets/dashboard_stats.dart';
-export '../dashboard/widgets/dashboard_activities.dart';
-export '../dashboard/widgets/dashboard_actions.dart';
